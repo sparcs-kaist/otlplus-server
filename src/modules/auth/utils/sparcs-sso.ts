ef8d6a616793d39ea4d56efd7b3228e88a0497d3
양지웅 <yangjiung@yangjiung-ui-noteubug.local>
2023-05-12 07:37:05 +0900

    signature sparcs-sso.ts

--- src/modules/auth/utils/sparcs-sso.ts
+++ src/modules/auth/utils/sparcs-sso.ts
@@ -29,8 +29,37 @@ export class Client {
       this.URLS[k] = base_url + this.URLS[k];
     }
     this.client_id = client_id;
-    this.secret_key = secret_key.encode();
+    this.secret_key = encodeURIComponent(secret_key);
   }
 
-  _sign_payload(payload, append_timestamp = true) {}
+  _sign_payload(payload: Array<string | number>, append_timestamp = true): any {
+    const timestamp = Math.floor(Date.now() / 1000);
+    if (append_timestamp) {
+      payload.push(timestamp);
+    }
+  }
+
+  _validate_sign(
+    payload: Array<string | number>,
+    timestamp: number,
+    sign: any,
+  ): any {}
+
+  _post_data(url: any, data: any): any {}
+  get_login_params(): any {}
+  get_user_info(code: any): any {}
+  get_logout_url(sid: any, redirect_uri: any): any {}
+  get_point(sid: any): any {}
+  modify_point(
+    sid: any,
+    delta: any,
+    message: any,
+    lower_bound: number = 0,
+  ): any {}
+  get_notice(
+    offset: number = 0,
+    limit: number = 3,
+    date_after: number = 0,
+  ): any {}
+  parse_unregister_request(data_dict: any): any {}
 }
