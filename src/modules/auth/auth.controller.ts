427a3cce410116bda31b4bc4e06bef60203941a9
Jiuuung <kjg682534@kaist.ac.kr>
2023-05-26 16:34:08 +0900

    Add:user_login router

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -8,17 +8,16 @@ import {
   Res,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { Request, Response} from "express";
-import { Client } from "./utils/sparcs-sso";
-import settings from "../../settings";
-import { UserService } from "../user/user.service";
-import { Public } from "../../common/decorators/skip-auth.decorator";
-import { GetUser } from "../../common/decorators/get-user.decorator";
-import { session_userprofile } from "@prisma/client";
+import { Request, Response } from 'express';
+import { Client } from './utils/sparcs-sso';
+import settings from '../../settings';
+import { UserService } from '../user/user.service';
+import { Public } from '../../common/decorators/skip-auth.decorator';
+import { GetUser } from '../../common/decorators/get-user.decorator';
+import { session_userprofile } from '@prisma/client';
 
 @Controller('session')
 export class AuthController {
-
   private readonly ssoClient;
 
   constructor(
@@ -26,7 +25,11 @@ export class AuthController {
     private readonly userService: UserService,
   ) {
     const ssoConfig = settings().getSsoConfig();
-    const ssoClient = new Client(ssoConfig.ssoClientId, ssoConfig.ssoSecretKey, ssoConfig.ssoIsBeta)
+    const ssoClient = new Client(
+      ssoConfig.ssoClientId,
+      ssoConfig.ssoSecretKey,
+      ssoConfig.ssoIsBeta,
+    );
     this.ssoClient = ssoClient;
   }
 
@@ -43,22 +46,27 @@ export class AuthController {
     @Req() req,
     @Res() res,
   ) {
-    req.session['next'] = next ? req.param.next : '/';
-    social_login = social_login ? social_login : 0;
-    /*let login_url,
-      state = sso_client.get_login_params();
-    if (social_login === 0) {
-    }*/
+    if (req.user && req.user.is_authenticated) {
+      return res.redirect(next ?? '/');
+    }
+    req.session['next'] = next ?? '/';
+    social_login = social_login ?? null;
+    let [login_url, state] = this.ssoClient.get_login_params();
+    if (social_login === '0') {
+      login_url += '&social_enabled=0&show_disabled_button=0';
+    }
+    req.session['sso_state'] = state;
+    return res.redirect(login_url);
   }
 
   @Public()
   @Get('login/callback')
-  async loginCallback(@Req() request: Request, @Res() response: Response){
-    const session:any = request.session
+  async loginCallback(@Req() request: Request, @Res() response: Response) {
+    const session: any = request.session;
     const stateBefore = session.sso_state ?? null;
     const state = request.query.state ?? null;
-    if (!stateBefore || stateBefore != state){
-      response.redirect('/error/invalid-login')
+    if (!stateBefore || stateBefore != state) {
+      response.redirect('/error/invalid-login');
     }
 
     const code = request.query.code ?? null;
@@ -68,27 +76,33 @@ export class AuthController {
     let user = await this.authService.findBySid(sid);
 
     const kaistInfo = JSON.parse(ssoProfile['kaist_info']);
-    const studentId = kaistInfo.get('ku_std_no') ?? "";
+    const studentId = kaistInfo.get('ku_std_no') ?? '';
 
-
-    if(!user){
-      user = await this.authService.createUser(sid, ssoProfile['email'], studentId, ssoProfile['first_name'], ssoProfile['last_name']);
-    }else{
-
-      if(user.student_id != studentId){
+    if (!user) {
+      user = await this.authService.createUser(
+        sid,
+        ssoProfile['email'],
+        studentId,
+        ssoProfile['first_name'],
+        ssoProfile['last_name'],
+      );
+    } else {
+      if (user.student_id != studentId) {
         await this.userService.import_student_lectures(studentId);
       }
 
       const updateData = {
-        first_name : ssoProfile['first_name'],
-        last_name : ssoProfile['last_name'],
-        student_id : studentId,
-      }
+        first_name: ssoProfile['first_name'],
+        last_name: ssoProfile['last_name'],
+        student_id: studentId,
+      };
       user = await this.authService.updateUser(user.id, updateData);
     }
 
-    const {accessToken, ...accessTokenOptions } = this.authService.getCookieWithAccessToken(user)
-    const {refreshToken, ...refreshTokenOptions } = this.authService.getCookieWithRefreshToken(user)
+    const { accessToken, ...accessTokenOptions } =
+      this.authService.getCookieWithAccessToken(user);
+    const { refreshToken, ...refreshTokenOptions } =
+      this.authService.getCookieWithRefreshToken(user);
 
     /*
     @Todo
@@ -108,7 +122,7 @@ export class AuthController {
   }
 
   @Get('info')
-  async getUserProfile(@GetUser() user: session_userprofile){
+  async getUserProfile(@GetUser() user: session_userprofile) {
     /*
     @Todo
     implement userSerializer, before that, we'd like to architect the dto types
