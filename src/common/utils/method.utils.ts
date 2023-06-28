2651da5ce46272c0f7a70d6911465776e4dd456e
LarryKwon <65128957+LarryKwon@users.noreply.github.com>
2023-06-29 00:16:28 +0900

    Add: reType the select result of user

--- src/common/utils/method.utils.ts
+++ src/common/utils/method.utils.ts
@@ -15,5 +15,15 @@ export function normalizeArray<T>(
   return normalizeObj
 }
 
+type ValueType = string | number | boolean
+export type Union<
+  T extends { [key: string]: ValueType } | ReadonlyArray<ValueType>,
+  > = T extends ReadonlyArray<ValueType>
+  ? T[number]
+  : T extends { [key: string]: infer U }
+    ? U
+    : never
+
+
 
 
