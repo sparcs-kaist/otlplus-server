9640d21e4df0718c363fa2ba50bf560db47df170
LarryKwon <65128957+LarryKwon@users.noreply.github.com>
2023-07-10 21:43:18 +0900

    Chore: Add DepartmentRepositiory to PrismaModule

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -3,15 +3,20 @@ import { PrismaService } from "./prisma.service";
 import { UserRepository } from "./repositories/user.repository";
 import { LectureRepository } from "./repositories/lecture.repository";
 import { ReviewRepository } from "./repositories/review.repository";
+import { DepartmentRepository } from "./repositories/department.repository";
 
 @Module({
   providers: [PrismaService,
     UserRepository,
     LectureRepository,
-    ReviewRepository],
+    ReviewRepository,
+    DepartmentRepository,
+  ],
   exports: [PrismaService,
     UserRepository,
     LectureRepository,
-    ReviewRepository]
+    ReviewRepository,
+    DepartmentRepository,
+  ]
 })
 export class PrismaModule {}
