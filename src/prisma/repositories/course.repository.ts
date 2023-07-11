052eb0a0edeb651f7b0f98e4768d664ad4bbff03
Dongjae Lee <58449555+duncan020313@users.noreply.github.com>
2023-07-11 19:41:07 +0900

    add_fk_between_course_and_department

--- src/prisma/repositories/course.repository.ts
+++ src/prisma/repositories/course.repository.ts
@@ -1,7 +1,5 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "../prisma.service";
-import { subject_course, subject_lecture } from "@prisma/client";
-import { courseSelectResultType } from "src/common/schemaTypes/types";
 
 @Injectable()
 export class CourseRepository{
@@ -38,7 +36,9 @@ export class CourseRepository{
       "CH",
       "TS",
   ]
-
+ //할일
+ // Foreign key subject_course_professor 새로 걸어주기
+ // subject_professor_course_list drop 하기
   public async filterByRequest (query: any) {
     const {
       department,
@@ -57,34 +57,30 @@ export class CourseRepository{
     const group_filter = this.group_filter(group);
     const keyword_filter = this.keyword_filter(keyword);
     const term_filter = this.term_filter(term);
+    let filter_list = [department_filter, type_filter, level_filter, group_filter, keyword_filter, term_filter]
+    filter_list = filter_list.filter((filter) => filter !== null)
     return await this.prisma.subject_course.findMany({
       include: {
         department: true,
+        professor_course_list: true
       },
       where: {
-        AND: [
-          department_filter,
-          type_filter,
-          level_filter,
-          group_filter,
-          keyword_filter,
-          term_filter,
-        ]
+        AND: filter_list
       }
     })
   }
 
-  private department_filter(department: [string]) {
-    if (!(department)) {
-      return {}
+  private department_filter(department_names: [string]): object {
+    if (!(department_names)) {
+      return null
     }
-    if ("ALL" in department) {
-      return {}
-    } else if ("ETC" in department) {
+    if ("ALL" in department_names) {
+      return null
+    } else if ("ETC" in department_names) {
       return {
         department: {
           code: {
-            notIn: { department }
+            notIn: { department_names }
           }
         }
       }
@@ -92,20 +88,20 @@ export class CourseRepository{
       return {
         department: {
           code: {
-            in: { department }
+            in: { department_names }
           }
         }
       }
     }
   }
 
-  private type_filter(types: [string]) {
+  private type_filter(types: [string]): object {
     if (!(types)) {
-      return {}
+      return null
     }
 
     if ("ALL" in types) {
-      return {}
+      return null
     } else if ("ETC" in types) {
       const unselected_types = Object.values(this.TYPE_ACRONYMS).filter((type) => !(type in types))
       return {
@@ -122,14 +118,14 @@ export class CourseRepository{
     }
   }
 
-  private level_filter(levels?: [string]) {
+  private level_filter(levels?: [string]): object {
     if (!(levels)) {
-      return {}
+      return null;
     }
 
     const acronym_dic = ["1", "2", "3", "4"];
     if ("ALL" in levels) {
-      return {}
+      return null;
     } else if ("ETC" in levels) {
       const numbers = acronym_dic.filter((level) => !(level in levels));
       return {
@@ -151,41 +147,83 @@ export class CourseRepository{
     }
   }
 
-  private term_filter(term?: [string]) {
+  private term_filter(term?: [string]): object {
     if (!(term)) {
-      return {}
+      return null;
     }
 
     if ("ALL" in term) {
-      return {}
+      return null;
     } else {
       const current_year = new Date().getFullYear().toString();
       return {
         lecture: {
           year: current_year
         }
-      }
+      };
     }
   }
 
-  private keyword_filter(keyword?: [string]) {
+  private keyword_filter(keyword?: [string]): object {
     if (!(keyword)) {
-      return {}
+      return null;
     }
 
     const keyword_trimed = keyword.map((word) => word.trim());
-    const keyword_space_removed = keyword.map((word) => word.replace(/\s/g, ""));
+    const keyword_space_removed = keyword_trimed.map((word) => word.replace(/\s/g, ""));
     const title_filter = {
-      title: {
-        
+      title_no_space: {
+        contains: { keyword_space_removed }
+      }
+    };
+    const en_title_filter = {
+      en_title_np_space: {
+        contains: { keyword_space_removed }
+      }
+    };
+    const department_name_filter = {
+      department: {
+        name: keyword_trimed 
+      }
+    };
+    const department_name_en_filter = {
+      department: {
+        name_en: keyword_trimed 
+      }
+    };
+    const professors_professor_name_filter = {
+      subject_professor_course_list: {
+        subject_professor: {
+          professor_name: {
+            contains: { keyword_trimed }
+          }
+        }
+      }
+    };
+    const professors_professor_name_en_filter = {
+      subject_professor_course_list: {
+        subject_professor: {
+          professor_name_en: {
+            contains: { keyword_trimed }
+          }
+        }
       }
     }
-    return
+    return {
+      OR: [
+        title_filter,
+        en_title_filter,
+        department_name_filter,
+        department_name_en_filter,
+        professors_professor_name_filter,
+        professors_professor_name_en_filter
+      ]
+    };
   }
 
-  private group_filter(group?: [string]) {
+  private group_filter(group?: [string]): object {
     if (!(group)) {
-      return {}
+      return null;
     }
 
     const filter = [];
