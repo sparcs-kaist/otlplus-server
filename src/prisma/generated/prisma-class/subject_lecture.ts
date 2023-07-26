5f5bc3296c5c61a5b30fea93cf9c02bbbb3e4d67
Dongjae Lee <58449555+duncan020313@users.noreply.github.com>
2023-07-26 19:28:03 +0900

    Migrate: add no space columns for lectures

--- src/prisma/generated/prisma-class/subject_lecture.ts
+++ src/prisma/generated/prisma-class/subject_lecture.ts
@@ -32,7 +32,7 @@ export class subject_lecture {
   department_id: number;
 
   @ApiProperty({ type: () => subject_department })
-  department: subject_department;
+  subject_department: subject_department;
 
   @ApiProperty({ type: String })
   class_no: string;
@@ -54,6 +54,12 @@ export class subject_lecture {
 
   @ApiProperty({ type: Number })
   credit: number;
+
+  @ApiProperty({ type: String })
+  title_en_no_space: string;
+
+  @ApiProperty({ type: String })
+  title_no_space: string;
 
   @ApiProperty({ type: Number })
   num_classes: number;
