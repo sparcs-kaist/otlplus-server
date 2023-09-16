a5eb9ebc71c0eaa318cacc0ae3e266e0a856647a
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2023-09-16 17:44:49 +0900

    fix: unwrap function without writing type

--- src/common/interfaces/dto/lecture/lecture.request.dto.ts
+++ src/common/interfaces/dto/lecture/lecture.request.dto.ts
@@ -5,35 +5,35 @@ import { CourseQueryDto } from '../course/course.request.dto';
 export class LectureQueryDto extends CourseQueryDto {
   @IsOptional()
   @Transform(({ value }) => (typeof value === 'string' ? [value] : value))
-  @Transform(({ value }) => value.map((v) => parseInt(v)))
+  @Transform(({ value }) => value.map(parseInt))
   @IsArray()
   @IsNumber({}, { each: true })
   year?: number[];
 
   @IsOptional()
   @Transform(({ value }) => (typeof value === 'string' ? [value] : value))
-  @Transform(({ value }) => value.map((v) => parseInt(v)))
+  @Transform(({ value }) => value.map(parseInt))
   @IsArray()
   @IsNumber({}, { each: true })
   semester?: number[];
 
   @IsOptional()
   @Transform(({ value }) => (typeof value === 'string' ? [value] : value))
-  @Transform(({ value }) => value.map((v) => parseInt(v)))
+  @Transform(({ value }) => value.map(parseInt))
   @IsArray()
   @IsNumber({}, { each: true })
   day?: number[];
 
   @IsOptional()
   @Transform(({ value }) => (typeof value === 'string' ? [value] : value))
-  @Transform(({ value }) => value.map((v) => parseInt(v)))
+  @Transform(({ value }) => value.map(parseInt))
   @IsArray()
   @IsNumber({}, { each: true })
   begin?: number[];
 
   @IsOptional()
   @Transform(({ value }) => (typeof value === 'string' ? [value] : value))
-  @Transform(({ value }) => value.map((v) => parseInt(v)))
+  @Transform(({ value }) => value.map(parseInt))
   @IsArray()
   @IsNumber({}, { each: true })
   end?: number[];
