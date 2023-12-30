e190a9654fe854c2d5e852dc1845e9474b1f52d6
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2023-12-30 11:42:07 +0900

    feat: implement without detail feed

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -2,7 +2,10 @@ import { Controller, Get, HttpException, Param, Query } from '@nestjs/common';
 import { session_userprofile } from '@prisma/client';
 import { GetUser } from 'src/common/decorators/get-user.decorator';
 import { CourseResponseDtoNested } from 'src/common/interfaces/dto/course/course.response.dto';
-import { UserTakenCoursesQueryDto } from 'src/common/interfaces/dto/user/user.request.dto';
+import {
+  UserFeedsQueryDto,
+  UserTakenCoursesQueryDto,
+} from 'src/common/interfaces/dto/user/user.request.dto';
 import { UserService } from './user.service';
 
 @Controller('api/users')
@@ -20,5 +23,18 @@ export class UserController {
     } else {
       throw new HttpException("Can't find user", 401);
     }
+  }
+
+  @Get(':user_id/feeds')
+  async getUserFeeds(
+    /**
+     * @todo use user by auth instead of userId by endpoint param
+     * userId should be removed from endpoint in the future
+     * since each user should only control their own timetable
+     */
+    @Query() query: UserFeedsQueryDto,
+    @GetUser() user: session_userprofile,
+  ): Promise<void> {
+    return await this.userService.getUserFeeds(query, user);
   }
 }
