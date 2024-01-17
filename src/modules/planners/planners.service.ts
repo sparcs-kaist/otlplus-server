4770f5433b7e4b1f9dce2178ef378d67243b39e1
Dongjae Lee <58449555+duncan020313@users.noreply.github.com>
2024-01-17 23:21:41 +0900

    feat: add basics for planner

--- src/modules/planners/planners.service.ts
+++ src/modules/planners/planners.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { session_userprofile } from '@prisma/client';
+import { PlannerRepository } from 'src/prisma/repositories/planner.repository';
 
 @Injectable()
-export class PlannersService {}
+export class PlannersService {
+  constructor(private readonly plannerRepository: PlannerRepository) {}
+
+  public async getPlannerByUser(
+    query: PlannerRequestDTODefault,
+    user: session_userprofile,
+  ) {
+    return await this.plannerRepository.getPlannerByUser(query, user);
+  }
+}
