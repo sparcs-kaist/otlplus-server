3acce4af55c39e03bd2a9d2f40b1d8598e813eaf
ddungiii <ggcc503@gmail.com>
2024-01-20 04:12:16 +0900

    refactor: add data migrations in prisma migration script

--- src/prisma/migrations/0_init/migration.sql
+++ src/prisma/migrations/0_init/migration.sql
@@ -1,4 +1,17 @@
 -- CreateTable
+create table _prisma_migrations (
+    id varchar(36) not null primary key, 
+    checksum varchar(64) not null, 
+    finished_at datetime(3) null, 
+    migration_name varchar(255) not null, 
+    logs text null, rolled_back_at datetime(3) null, 
+    started_at datetime(3) default CURRENT_TIMESTAMP(3) not null, 
+    applied_steps_count int unsigned default 0 not null
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+
+
+-- CreateTable
 CREATE TABLE `auth_group` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(150) NOT NULL,
