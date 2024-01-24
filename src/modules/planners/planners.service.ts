3d65d61ca29c8271a3c2f9eb9fd8703061e27fce
Dongjae Lee <58449555+duncan020313@users.noreply.github.com>
2024-01-24 20:26:57 +0900

    fix: fix typo

--- src/modules/planners/planners.service.ts
+++ src/modules/planners/planners.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { session_userprofile } from '@prisma/client';
+import { PlannerRequestDtoDefault } from 'src/common/interfaces/dto/planner/planner.request.dto';
 import { PlannerRepository } from 'src/prisma/repositories/planner.repository';
 
 @Injectable()
@@ -7,7 +8,7 @@ export class PlannersService {
   constructor(private readonly plannerRepository: PlannerRepository) {}
 
   public async getPlannerByUser(
-    query: PlannerRequestDTODefault,
+    query: PlannerRequestDtoDefault,
     user: session_userprofile,
   ) {
     return await this.plannerRepository.getPlannerByUser(query, user);
