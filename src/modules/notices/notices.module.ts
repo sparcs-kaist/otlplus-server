be4c5b119b2f6e12a5cc7e8c8b3bea04f7546b98
Giyeong Kim <ggcc503@gmail.com>
2024-02-28 22:21:01 +0900

    feat: add get notices (#74)
    * feat: implement without detail feed
    
    * chore: add prisma generate on ci
    
    * chore: fix prisma-generate command
    
    * chore: remove ci branch on target branches
    
    * chore: rename generating step name
    
    * refactor: prisma scripts
    
    * fix: cors origin port
    
    * fix: db init script
    
    * refactor: add data migrations in prisma migration script
    
    * fix: generate script in ci
    
    * chore: Add cross-env (#64)
    
    * fix: a bug that orderFilter return empty filters (#65)
    
    * feat: add feeds module
    
    * feat: move feeds api to feeds module
    
    * feat: add IFeed
    
    * feat: add feed repository and serializer
    
    * feat: add get famousHumanityFeeds
    
    * feat: make random select HumanityBestReviews
    
    * refactor: change class to interface
    
    * feat: add rankedReviewDailyFeed
    
    * feat: add famous major review daily feed
    
    * feat: add review write daily user feed
    
    * feat: add ICourse
    
    * feat: add related course daily user feed
    
    * feat: add rate daily user feed
    
    * feat: add const FeedType
    
    * refactor: add FeedSchema namespace
    
    * refactor: add details interface in IFeed
    
    * refactor: rename redundant feed names
    
    * refactor: add Feed constants
    
    * fix: fix related course
    
    * refactor: fix typos
    
    * feat: add writable review
    
    * feat: comment out related course
    
    * refactor: move ICourse to structures
    
    * refactor: move directory interfaces
    
    * refactor: rename schemaTypes to entities
    
    * refactor: remove redundant prefix I
    
    * refactor: remove redundant prefix E
    
    * refactor: remove redundant prefix E
    
    * feat: add get notices
    
    * refactor: move repository to prisma module
    
    * refaoctor: map funciton
    
    * fix: add repository to exports in module
    
    ---------
    
    Co-authored-by: JeukHwang <92910273+JeukHwang@users.noreply.github.com>
    Co-authored-by: doxylee <68041124+doxylee@users.noreply.github.com>

--- src/modules/notices/notices.module.ts
+++ src/modules/notices/notices.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { NoticesController } from './notices.controller';
 import { NoticesService } from './notices.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [NoticesController],
   providers: [NoticesService],
 })
