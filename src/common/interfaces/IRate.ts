1c61d3b18248c78c235a232e67b65249863b2fc1
Giyeong Kim <ggcc503@gmail.com>
2024-03-06 22:54:01 +0900

    feat: add rates API (#51)
    * feat: init rate module
    
    * chore: lock new package version
    
    * fix: add endpoint prefix
    
    * fix: rate module
    
    * fix: add created_datetime
    
    * refactor: ICommon -> IBasic
    
    * refactor: remove prefix I in Basic
    
    * fix: package verion supporting sementic versioning
    
    * feat: add default date for rate

---
+++ src/common/interfaces/IRate.ts
@@ -0,0 +1,20 @@
+import { Type } from 'class-transformer';
+import { IsNumber, Max, Min } from 'class-validator';
+
+export namespace IRate {
+  export interface Basic {
+    id: number;
+    user_id: number;
+    score: number;
+    version: string;
+    created_datetime: Date | null;
+  }
+
+  export class CreateDto {
+    @IsNumber()
+    @Max(5)
+    @Min(1)
+    @Type(() => Number)
+    score!: number;
+  }
+}
