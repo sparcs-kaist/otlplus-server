a254526189447a42e76094de3ffbfb394866be5d
doxylee <doxylee@sparcs.org>
2024-07-16 15:57:21 +0900

    refactor: PlannerDetails

--- src/common/schemaTypes/types.ts
+++ src/common/schemaTypes/types.ts
@@ -1,23 +1,6 @@
 import { Prisma } from '@prisma/client';
 import { ECourse } from '../entities/ECourse';
 import { ELecture } from '../entities/ELecture';
-import { EPlanners } from '../entities/EPlanners';
-import { ETrack } from '../entities/ETrack';
-
-export const plannerDetails = Prisma.validator<Prisma.planner_plannerArgs>()({
-  include: {
-    planner_planner_additional_tracks: {
-      include: {
-        graduation_additionaltrack: ETrack.Additional,
-      },
-    },
-    graduation_generaltrack: true,
-    graduation_majortrack: ETrack.Major,
-    planner_takenplanneritem: EPlanners.EItems.Taken.Details,
-    planner_arbitraryplanneritem: EPlanners.EItems.Arbitrary.Extended,
-    planner_futureplanneritem: EPlanners.EItems.Future.Extended,
-  },
-});
 
 export type NESTED = true;
 
@@ -61,10 +44,6 @@ export type ReviewDetails = Prisma.review_reviewGetPayload<
 export type LectureBasic = Prisma.subject_lectureGetPayload<null>;
 export type TimeTableBasic = Prisma.timetable_timetableGetPayload<null>;
 export type SemesterBasic = Prisma.subject_semesterGetPayload<null>;
-export type PlannerBasic = Prisma.planner_plannerGetPayload<null>;
-export type PlannerDetails = Prisma.planner_plannerGetPayload<
-  typeof plannerDetails
->;
 
 export type WishlistWithLectures = Prisma.timetable_wishlistGetPayload<
   typeof wishlistWithLectures
