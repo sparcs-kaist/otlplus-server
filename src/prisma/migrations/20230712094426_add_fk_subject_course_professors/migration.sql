6441c476cac60d28e42cc83fae51969abf7d595d
LarryKwon <65128957+LarryKwon@users.noreply.github.com>
2024-08-22 01:38:33 +0900

    128 prisma migration 정리 (#129)
    * Add: implement Auth Chain
    
    todo:
    AuthModule Import and register
    
    * prisma migration 정리
    
    * fix: migration sql
    
    * fix: prisma schema
    
    ---------
    
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-40-122.ap-northeast-2.compute.internal>

--- src/prisma/migrations/20230712094426_add_fk_subject_course_professors/migration.sql
+++
@@ -1,5 +0,0 @@
--- AddForeignKey
-ALTER TABLE `subject_course_professors` ADD CONSTRAINT `subject_course_professors_course_id_fkey` FOREIGN KEY (`course_id`) REFERENCES `subject_course`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
--- AddForeignKey
-ALTER TABLE `subject_course_professors` ADD CONSTRAINT `subject_course_professors_professor_id_fkey` FOREIGN KEY (`professor_id`) REFERENCES `subject_professor`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
--- src/prisma/migrations/20230717112040_add_fk_subject_course_subject_lecture/migration.sql
+++
@@ -1,2 +0,0 @@
--- AddForeignKey
-ALTER TABLE `subject_lecture` ADD CONSTRAINT `subject_lecture_course_id_fkey` FOREIGN KEY (`course_id`) REFERENCES `subject_course`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
