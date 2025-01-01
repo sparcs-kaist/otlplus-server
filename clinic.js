cb4d9fbce0df64bcd516afd4e4a91b58f59d4437
LarryKwon <65128957+LarryKwon@users.noreply.github.com>
2025-01-01 16:34:45 +0900

    memory leak analysis

---
+++ clinic.js
@@ -0,0 +1,20 @@
+const ClinicHeapProfiler = require('@clinic/heap-profiler');
+
+const heapProfiler = new ClinicHeapProfiler();
+
+// ['node', '--nodeoption', 'app.js']로 옵션 추가 가능
+heapProfiler.collect(
+  ['node', 'dist/src/bootstrap/bootstrap.js'],
+  (err, filepath) => {
+    if (err) {
+      console.log('collect error\n', err);
+    }
+
+    console.log(filepath);
+    heapProfiler.visualize(filepath, `${filepath}.html`, () => {
+      if (err) {
+        console.log('visualize error\n', err);
+      }
+    });
+  },
+);
