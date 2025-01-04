bdd9f99a6fabaf1efb63b26bee1b21fea26788f4
Gerbera3090 <hw3090@gmail.com>
2025-01-04 22:39:42 +0900

    fix: course search adjust to new code

--- src/prisma/repositories/course.repository.ts
+++ src/prisma/repositories/course.repository.ts
@@ -312,11 +312,18 @@ export class CourseRepository {
         contains: keyword_space_removed,
       },
     };
+
+    const new_code_filter = {
+      new_code: {
+        contains: this.formatNewLectureCodeWithDot(keyword_space_removed),
+      },
+    };
     return {
       OR: [
         title_filter,
         en_title_filter,
         old_code_filter,
+        new_code_filter,
         department_name_filter,
         department_name_en_filter,
         professors_professor_name_filter,
@@ -485,5 +492,23 @@ export class CourseRepository {
       courseUser.subject_course.latest_written_datetime >
       courseUser.latest_read_datetime
     );
+  }
+
+  formatNewLectureCodeWithDot(keyword: string): string {
+    // new_code의 .을 수용할 수 있게 과목코드를 바꿔주는 함수
+    // utils/search.utils 로 옮길까요??
+    // OR public 이나 private 등의 기타 요건이 필요할까요??
+
+    // 정규식: [알파벳]+[숫자]+ 형식 매칭
+    const regex = /^([a-zA-Z]+)(\d+)$/;
+
+    // 키워드가 정규식에 매칭되면 변환, 매칭되지 않으면 원래 값을 반환
+    const match = keyword.match(regex);
+    if (match) {
+      const [, letters, numbers] = match; // 알파벳 그룹과 숫자 그룹 추출
+      return `${letters}.${numbers}`; // 알파벳과 숫자 사이에 '.' 삽입
+    }
+
+    return keyword; // 변환 불가능한 경우 원래 키워드 반환
   }
 }
