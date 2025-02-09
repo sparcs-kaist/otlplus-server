dcd0bb9c001c0aa6d1550c35a18da1c5bd9c6cec
LarryKwon <65128957+LarryKwon@users.noreply.github.com>
2025-02-09 19:50:11 +0900

    change strategy to join

--- apps/load-test/get-lecture-test.ts
+++ apps/load-test/get-lecture-test.ts
@@ -3,9 +3,9 @@ import { check, sleep } from 'k6';
 
 export const options = {
   stages: [
-    { duration: '1m', target: 100 }, // 1분 동안 100명의 VU로 증가
-    { duration: '1m', target: 200 }, // 3분 동안 200명의 VU 유지
-    { duration: '1m', target: 0 }, // 1분 동안 0명의 VU로 감소
+    { duration: '1m', target: 30 }, // 1분 동안 100명의 VU로 증가
+    { duration: '1m', target: 50 }, // 3분 동안 200명의 VU 유지
+    { duration: '1m', target: 15 }, // 1분 동안 0명의 VU로 감소
   ],
   thresholds: {
     http_req_duration: ['p(95)<1000'], // 95%의 요청이 1초 이내에 응답해야 함
@@ -15,7 +15,7 @@ export const options = {
 
 export default function () {
   const url =
-    'http://localhost:8000/api/lectures?year=2024&semester=3&keyword=&type=HSE&department=ALL&level=ALL&order=old_code&order=class_no&limit=300&isFull=True';
+    'https://api.otl.dev.sparcs.org/api/lectures?year=2024&semester=3&keyword=&type=HSE&department=ALL&level=ALL&order=old_code&order=class_no&limit=300&isFull=True';
   const res = http.get(url);
 
   check(res, {
