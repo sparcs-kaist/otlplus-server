75aa680cedab90ea852877662fb20f1c5b441579
aariprom <jseokp4@gmail.com>
2025-03-06 21:05:15 +0900

    refactor: migration squashed && add: paper_professor_department table

--- apps/server/src/prisma/migrations/20250227180638_lab_tf_one_more_fix/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `professor_name` to the `paper` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `paper` ADD COLUMN `professor_name` VARCHAR(255) NOT NULL;
--- apps/server/src/prisma/migrations/20250227181458_labtf_null_constraint_fix_2/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE `paper` MODIFY `title` VARCHAR(300) NULL,
-    MODIFY `abstract` TEXT NULL,
-    MODIFY `keywords` TEXT NULL,
-    MODIFY `category` VARCHAR(100) NULL,
-    MODIFY `doi` VARCHAR(100) NULL,
-    MODIFY `pdf_link` VARCHAR(255) NULL,
-    MODIFY `xml_link` VARCHAR(255) NULL,
-    MODIFY `url` VARCHAR(255) NULL;
