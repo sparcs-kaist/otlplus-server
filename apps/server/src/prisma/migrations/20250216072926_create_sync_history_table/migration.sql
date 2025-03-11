71c7ab3dc7f8ab5a9f686f4fdf5d2f4fee7e7bfa
LarryKwon <65128957+LarryKwon@users.noreply.github.com>
2025-03-11 11:43:44 +0900

    hotfix: make sync_history table's data field as nullable

--- apps/server/src/prisma/migrations/20250216072926_create_sync_history_table/migration.sql
+++ apps/server/src/prisma/migrations/20250216072926_create_sync_history_table/migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE `sync_history` (
     `type` ENUM('DEPARTMENT', 'COURSE', 'LECTURE', 'PROFESSOR', 'MAJORS', 'DEGREE','EXAMTIME','CLASSTIME','TAKEN_LECTURES','CHARGE') NOT NULL,
     `startTime` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
     `endTime` DATETIME(0),
-    `data` VARCHAR(191) NOT NULL,
+    `data` VARCHAR(191),
     `year` INTEGER,
     `semester` INTEGER,
     PRIMARY KEY (`id`)
