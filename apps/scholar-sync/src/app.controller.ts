f4cb28a62cfdfeb635e3756fe2c1161f53f485fa
LarryKwon <65128957+LarryKwon@users.noreply.github.com>
2025-03-11 14:01:34 +0900

    add hello endpoint to /api

--- apps/scholar-sync/src/app.controller.ts
+++ apps/scholar-sync/src/app.controller.ts
@@ -11,4 +11,10 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/api')
+  @Public()
+  getApiHello(): string {
+    return this.appService.getHello();
+  }
 }
