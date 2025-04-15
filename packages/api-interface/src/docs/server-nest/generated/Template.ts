0b8c4114b2b32b82ae85683105db4403bc3ba9bf
LarryKwon <65128957+LarryKwon@users.noreply.github.com>
2025-04-15 18:12:33 +0900

    Issue/186/api doc (#188)
    * add: api-doc gen
    
    * add: index.ts

---
+++ packages/api-interface/src/docs/server-nest/generated/Template.ts
@@ -0,0 +1,17 @@
+import { IAuth } from "@otl/api-interface/src/interfaces";
+import { IUser } from "@otl/api-interface/src/interfaces";
+import { ICourse } from "@otl/api-interface/src/interfaces";
+import { ILecture } from "@otl/api-interface/src/interfaces";
+import { IReview } from "@otl/api-interface/src/interfaces";
+import { IFeed } from "@otl/api-interface/src/interfaces";
+import { INotice } from "@otl/api-interface/src/interfaces";
+import { IDepartment } from "@otl/api-interface/src/interfaces";
+import { ISession } from "@otl/api-interface/src/interfaces";
+import { IShare } from "@otl/api-interface/src/interfaces";
+import { IPlanner } from "@otl/api-interface/src/interfaces/IPlanner";
+import { IRate } from "@otl/api-interface/src/interfaces/IRate";
+import { ISemester } from "@otl/api-interface/src/interfaces/ISemester";
+import { ISync } from "@otl/api-interface/src/interfaces/ISync";
+
+export namespace Template {
+}
