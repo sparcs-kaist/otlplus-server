617ef2d46271fe8b2b065f294b9fcdfb618902ce
LarryKwon <65128957+LarryKwon@users.noreply.github.com>
2025-04-21 22:17:34 +0900

    make pm2 cluster instance to 1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,14 +3,14 @@ module.exports = {
     {
       name: '@otl/server-nest',
       script: './dist/apps/server/apps/server/src/bootstrap/bootstrap.js',
-      instances: 2,
+      instances: 1,
       exec_mode: 'cluster',
       merge_logs: true,
       autorestart: true,
       wait_ready: true,
       kill_timeout: 5000, // 새로운 프로세스 실행이 완료된 후 예전 프로세스를 교체하기까지 기다릴 시
-      max_memory_restart: '1024M',
-      node_args: '--max-old-space-size=2048',
+      max_memory_restart: '20048M',
+      node_args: '--max-old-space-size=3072',
     },
   ],
 };
