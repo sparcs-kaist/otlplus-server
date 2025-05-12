ada484b7d9202844177d89e525358cfe9ca007a5
LarryKwon <65128957+LarryKwon@users.noreply.github.com>
2025-05-13 04:22:58 +0900

    Add: add user tag on sentry message

--- libs/common/src/exception/exception.filter.ts
+++ libs/common/src/exception/exception.filter.ts
@@ -1,14 +1,13 @@
 import {
   ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus,
 } from '@nestjs/common'
-import { SentryExceptionCaptured } from '@sentry/nestjs'
+import { session_userprofile } from '@prisma/client'
 import * as Sentry from '@sentry/node'
 
 import logger from '../logger/logger'
 
 @Catch() // BaseException을 상속한 exception에 대해서 실행됨.
 export class UnexpectedExceptionFilter implements ExceptionFilter {
-  @SentryExceptionCaptured()
   catch(exception: unknown, host: ArgumentsHost) {
     const ctx = host.switchToHttp()
     const response = ctx.getResponse()
@@ -24,12 +23,35 @@ export class UnexpectedExceptionFilter implements ExceptionFilter {
       timestamp: new Date().toISOString(),
       path: request.url,
     })
+
+    Sentry.withIsolationScope((scope) => {
+      if (request.user) {
+        const user: session_userprofile = request.user as session_userprofile
+        scope.setUser({ sid: String(user.sid), email: user.email ?? '', studentId: user.student_id })
+      }
+
+      scope.setTag('method', request.method)
+      scope.setTag('url', request.originalUrl)
+      scope.setTag('status_code', response.statusCode)
+
+      scope.setContext('request', {
+        method: request.method,
+        url: request.originalUrl,
+        body: request.body,
+        query: request.query,
+        headers: request.headers,
+      })
+
+      scope.setContext('response', {
+        statusCode: response.statusCode,
+      })
+      Sentry.captureException(exception)
+    })
   }
 }
 
 @Catch(HttpException) // BaseException을 상속한 exception에 대해서 실행됨.
 export class HttpExceptionFilter<T extends HttpException> implements ExceptionFilter {
-  @SentryExceptionCaptured()
   catch(exception: T, host: ArgumentsHost) {
     const ctx = host.switchToHttp()
     const response = ctx.getResponse()
@@ -37,13 +59,36 @@ export class HttpExceptionFilter<T extends HttpException> implements ExceptionFi
 
     const resStatus = exception.getStatus()
     logger.error(exception.getResponse())
-    Sentry.captureException(exception)
+
     response.status(resStatus).json({
       // todo: exception의 response 형식 결정되면 변경해야함.
       message: exception.getResponse(), // test를 위한 코드
       statusCode: resStatus,
       timestamp: new Date().toISOString(),
       path: request.url,
+    })
+    Sentry.withIsolationScope((scope) => {
+      if (request.user) {
+        const user: session_userprofile = request.user as session_userprofile
+        scope.setUser({ sid: String(user.sid), email: user.email ?? '', studentId: user.student_id })
+      }
+
+      scope.setTag('method', request.method)
+      scope.setTag('url', request.originalUrl)
+      scope.setTag('status_code', response.statusCode)
+
+      scope.setContext('request', {
+        method: request.method,
+        url: request.originalUrl,
+        body: request.body,
+        query: request.query,
+        headers: request.headers,
+      })
+
+      scope.setContext('response', {
+        statusCode: 500,
+      })
+      Sentry.captureException(exception)
     })
   }
 }
