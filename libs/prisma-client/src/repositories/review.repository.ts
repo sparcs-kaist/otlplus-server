e2299b5364f5f30ada3c26ee53d844f27b180ee4
LarryKwon <65128957+LarryKwon@users.noreply.github.com>
2025-05-22 16:15:29 +0900

    Hotfix: query optimization for /api/reviews

--- libs/prisma-client/src/repositories/review.repository.ts
+++ libs/prisma-client/src/repositories/review.repository.ts
@@ -103,32 +103,16 @@ export class ReviewsRepository {
       orderDict[orderBy[orderBy.length - 1]] = sortOrder
       orderFilters.push(orderDict)
     })
-    const reviews: EReview.Details[] = await this.prisma.review_review.findMany({
+    console.log(offset, limit)
+    return this.prisma.review_review.findMany({
       where: {
         lecture: lectureFilter,
       },
       include: EReview.Details.include,
+      orderBy: orderFilters,
       skip: offset,
       take: limit,
-      orderBy: orderFilters,
-      distinct: [
-        'id',
-        'course_id',
-        'lecture_id',
-        'content',
-        'grade',
-        'load',
-        'speech',
-        'writer_id',
-        'writer_label',
-        'updated_datetime',
-        'like',
-        'is_deleted',
-        'written_datetime',
-      ],
     })
-
-    return reviews
   }
 
   async getReviewsOfLecture(id: number, order: string[], offset: number, limit: number): Promise<EReview.Details[]> {
