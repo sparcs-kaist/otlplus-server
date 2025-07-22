9cd821a8da79c5cc5a43d51d9823dd7dd0ff5e6c
LarryKwon <65128957+LarryKwon@users.noreply.github.com>
2025-07-22 22:35:24 +0900

    Add: synchronize the cache ttl with scholar sync

--- apps/server/src/modules/lectures/lectures.controller.ts
+++ apps/server/src/modules/lectures/lectures.controller.ts
@@ -1,13 +1,11 @@
 import { CacheInterceptor, CacheTTL } from '@nestjs/cache-manager'
 import {
-  Controller, Get, Param, Query, UseInterceptors,
+  Controller, ExecutionContext, Get, Param, Query, UseInterceptors,
 } from '@nestjs/common'
 import { GetUser } from '@otl/server-nest/common/decorators/get-user.decorator'
 import { Public } from '@otl/server-nest/common/decorators/skip-auth.decorator'
 import { ILecture, IReview } from '@otl/server-nest/common/interfaces'
 import { session_userprofile } from '@prisma/client'
-
-import { RedisTTL } from '@otl/common/enum/time'
 
 import { LecturesService } from './lectures.service'
 
@@ -15,8 +13,30 @@ import { LecturesService } from './lectures.service'
 export class LecturesController {
   constructor(private readonly LectureService: LecturesService) {}
 
+  private static cacheTTLFactory = (_context: ExecutionContext): number => {
+    const now = new Date()
+
+    // KST 기준 현재 시간
+    const KST_OFFSET = 9 * 60
+    const nowKST = new Date(now.getTime() + KST_OFFSET * 60 * 1000)
+
+    // 다음 정각 계산
+    const nextHourKST = new Date(nowKST)
+    nextHourKST.setMinutes(0, 0, 0) // 정각으로 초기화
+    nextHourKST.setHours(nowKST.getHours() + 1) // 다음 시간으로 이동
+
+    // TTL 계산 (ms 단위)
+    let ttlInMs = nextHourKST.getTime() - nowKST.getTime()
+
+    // Jitter: 최대 60초까지 감산
+    const jitterMs = Math.floor(Math.random() * 60_000) // 0~59,999 ms
+    ttlInMs += jitterMs
+
+    return ttlInMs
+  }
+
   @Public()
-  @CacheTTL(RedisTTL.HOUR)
+  @CacheTTL(LecturesController.cacheTTLFactory)
   @UseInterceptors(CacheInterceptor)
   @Get()
   async getLectures(@Query() query: ILecture.QueryDto): Promise<ILecture.Detail[]> {
@@ -24,7 +44,7 @@ export class LecturesController {
   }
 
   @Public()
-  @CacheTTL(RedisTTL.HOUR)
+  @CacheTTL(LecturesController.cacheTTLFactory)
   @UseInterceptors(CacheInterceptor)
   @Get('autocomplete')
   async getLectureAutocomplete(@Query() query: ILecture.AutocompleteQueryDto): Promise<string | undefined> {
@@ -32,7 +52,7 @@ export class LecturesController {
   }
 
   @Public()
-  @CacheTTL(RedisTTL.HOUR)
+  @CacheTTL(LecturesController.cacheTTLFactory)
   @UseInterceptors(CacheInterceptor)
   @Get(':id')
   async getLectureById(@Param('id') id: number): Promise<ILecture.Detail> {
