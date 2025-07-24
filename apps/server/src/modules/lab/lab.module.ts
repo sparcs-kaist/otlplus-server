b543675ef7a96d720864530475b50cb7b8995efd
aariprom <jseokp4@gmal.com>
2025-07-24 19:51:11 +0900

    fix: weaviateconfig

--- apps/server/src/modules/lab/lab.module.ts
+++ apps/server/src/modules/lab/lab.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
+import settings from '@otl/lab-server/settings'
 import { LabController } from '@otl/server-nest/modules/lab/lab.controller'
 import { LabService } from '@otl/server-nest/modules/lab/lab.service'
 import { WeaviateModule } from '@otl/weaviate-client/weaviate.module'
 
 @Module({
-  imports: [WeaviateModule.register()],
+  imports: [WeaviateModule.register(settings().weaviateConfig())],
   controllers: [LabController],
   providers: [LabService],
   exports: [LabService],
