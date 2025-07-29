ce1f9de5bc4abeaaf4d1f17a3372bfc881baae92
LarryKwon <65128957+LarryKwon@users.noreply.github.com>
2025-07-30 02:13:01 +0900

    Add: add temp console.log for test

--- libs/rmq/src/rabbitmq.publisher.ts
+++ libs/rmq/src/rabbitmq.publisher.ts
@@ -31,10 +31,11 @@ export class RabbitPublisherService {
 
     console.log(`Publishing to exchange: ${exchange}, routingKey: ${routingKey}, payload:`, payload)
 
-    channel.publish(exchange, routingKey, Buffer.from(JSON.stringify(payload)), {
+    const result = channel.publish(exchange, routingKey, Buffer.from(JSON.stringify(payload)), {
       persistent: true,
       contentType: 'application/json',
       headers: { 'x-delay': options.delay ?? 0 },
     })
+    console.log(`Publish result: ${result}`)
   }
 }
