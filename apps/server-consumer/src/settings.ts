430a4d3eceb67af05d610ea2e47803a937cde939
LarryKwon <65128957+LarryKwon@users.noreply.github.com>
2025-07-30 02:03:16 +0900

    Add: add console.log for test

--- apps/server-consumer/src/settings.ts
+++ apps/server-consumer/src/settings.ts
@@ -12,7 +12,7 @@ const getPrismaConnectConfig = (): mariadb.PoolConfig => ({
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  connectionLimit: 10,
+  connectionLimit: 5,
 })
 
 const getPrismaReadConnectConfig = (): mariadb.PoolConfig => ({
@@ -21,7 +21,7 @@ const getPrismaReadConnectConfig = (): mariadb.PoolConfig => ({
   user: process.env.READ_DATABASE_USER,
   password: process.env.READ_DATABASE_PASSWORD,
   database: process.env.READ_DATABASE_NAME,
-  connectionLimit: 10,
+  connectionLimit: 5,
 })
 const sentryConfig = () => ({
   dsn: process.env.SENTRY_DSN_SERVER_CONSUMER,
