826315b26751701e77f43c7c00a2c8a806d9bf9d
SeungBeom-Park <killerwhale@sparcs.org>
2025-09-26 21:29:12 +0900

    finish api - update migration file

--- libs/prisma-client/src/migrations/20250926113053_custom_block/migration.sql
+++ libs/prisma-client/src/migrations/20250926113053_custom_block/migration.sql
@@ -1,17 +1,19 @@
 -- AlterTable
+-- AlterTable
 ALTER TABLE `subject_course` MODIFY `level` VARCHAR(20) NULL;
 UPDATE `subject_course` SET level = SUBSTRING(REGEXP_REPLACE(new_code, '[^0-9]', ''), 1, 1);
 -- AlterTable
 ALTER TABLE `subject_lecture` MODIFY `level` VARCHAR(20) NULL;
 UPDATE `subject_lecture` SET level = SUBSTRING(REGEXP_REPLACE(new_code, '[^0-9]', ''), 1, 1);
+
 -- CreateTable
 CREATE TABLE `block_custom_blocks` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `block_name` VARCHAR(255) NOT NULL,
     `place` VARCHAR(255) NOT NULL,
     `day` INTEGER NOT NULL,
-    `begin` TIME(0) NOT NULL,
-    `end` TIME(0) NOT NULL,
+    `begin` INTEGER NOT NULL,
+    `end` INTEGER NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -31,4 +33,4 @@ CREATE TABLE `timetable_timetable_customblocks` (
 ALTER TABLE `timetable_timetable_customblocks` ADD CONSTRAINT `fk_timetable_timetable_custom__block_custom_blocks` FOREIGN KEY (`custom_block_id`) REFERENCES `block_custom_blocks`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 -- AddForeignKey
-ALTER TABLE `timetable_timetable_customblocks` ADD CONSTRAINT `fk_timetable_timetable_custom__timetable_timetalble` FOREIGN KEY (`timetable_id`) REFERENCES `timetable_timetable`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE `timetable_timetable_customblocks` ADD CONSTRAINT `fk_timetable_timetable_custom__timetable_timetable` FOREIGN KEY (`timetable_id`) REFERENCES `timetable_timetable`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
