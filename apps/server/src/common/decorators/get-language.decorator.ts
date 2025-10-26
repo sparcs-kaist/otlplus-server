5348e2da166a2776104ae168885043f82ef17150
cyber <cyber@sparcs.org>
2025-10-27 02:00:59 +0900

    fix: update language decorator to return 'en' for English requests

--- apps/server/src/common/decorators/get-language.decorator.ts
+++ apps/server/src/common/decorators/get-language.decorator.ts
@@ -2,5 +2,6 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common'
 
 export const GetLanguage = createParamDecorator((_data, ctx: ExecutionContext): string => {
   const req = ctx.switchToHttp().getRequest()
-  return req.headers['accept-language']?.toString() || 'kr'
+  if (req.headers['accept-language']?.toString().lower() === 'en') return 'en'
+  return 'kr'
 })
