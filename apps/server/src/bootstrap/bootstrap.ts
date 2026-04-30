e0f1f5713d3a4927304711d68eb71127e5a650aa
cyber <cyber@sparcs.org>
2026-04-30 22:18:10 +0900

    chore(sentry): don't log NOT_FOUND error

--- apps/server/src/bootstrap/bootstrap.ts
+++ apps/server/src/bootstrap/bootstrap.ts
@@ -48,7 +48,10 @@ async function bootstrap() {
     tracesSampleRate: 1.0,
     beforeSend(event, hint) {
       const error = hint?.originalException
-      if (error instanceof HttpException && error.getStatus() === HttpStatus.UNAUTHORIZED) {
+      if (
+        error instanceof HttpException
+        && (error.getStatus() === HttpStatus.UNAUTHORIZED || error.getStatus() === HttpStatus.NOT_FOUND)
+      ) {
         return null
       }
       return event
